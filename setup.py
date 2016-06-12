59a342741b7fbf525b839d70e6712ff20a06fb05
andela-rekemezie <rowland.ekemezie@andela.com>
2016-06-12 22:48:02 +0100

    Add initial setup configurations

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Portfolio',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='Application to allow users to create their online CV',
+      author='Rowland Ekemezie',
+      author_email='rowland.ekemezie@andela.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+      install_requires=[
+          'Flask',
+          'Flask-SQLAlchemy',
+          'Flask-Login',
+          'Flask-WTF'
+      ],
+      )
