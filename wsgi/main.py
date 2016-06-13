ea5b93085271cff1004049b483b1a85c36cc9bdf
andela-rekemezie <rowland.ekemezie@andela.com>
2016-06-13 14:17:08 +0100

    Add Jinja templating to application UI

--- wsgi/main.py
+++ wsgi/main.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
 
 @application.route('/')
-def index():
-    return 'This is the beginning of the test applicaton'
-
+@application.route('/<username>')
+def index(username=None):
+    if username is None:
+        return render_template("themes/water/index.html", page_title="Portfolio manager")
+    return render_template("themes/water/portfolio.html", page_title=username)
 
 if __name__ == '__main__':
     application.run(debug=True, host='0.0.0.0', port=9000)
