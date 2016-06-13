ee2edba31127bae5e90230b558550a1b70060e7d
andela-rekemezie <rowland.ekemezie@andela.com>
2016-06-13 11:21:35 +0100

    Remove unnecessary file extension

--- wsgi.py
+++ wsgi.py
@@ -12,3 +12,14 @@ except IOError:
 # line, it's possible required libraries won't be in your searchable path
 #
 
+from main import application
+
+#
+# Below for testing only
+#
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+
+    httpd = make_server('localhost', 8051, application)
+    # Wait for a single request, serve it and quit.
+    httpd.handle_request()
