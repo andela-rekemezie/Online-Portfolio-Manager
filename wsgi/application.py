cb4158f79e94a98194bc277c68d8ee7f93bb8c6a
andela-rekemezie <rowland.ekemezie@andela.com>
2016-06-13 05:20:44 +0100

    Remove redundant files

--- wsgi/application.py
+++ wsgi/application.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python
 import os
 
-# virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
+ virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+# virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
-    execfile(virtenv, dict(__file__=virtenv))
+    execfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
     pass
 
 from main import application
-
-# if __name__ == '__main__':
-#     from wsgiref.simple_server import make_server
-#     httpd = make_server('localhost', 8051, application)
-#     # Wait for a single request, serve it and quit.
-#     httpd.serve_forever()
