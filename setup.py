1c2d147c4e333eba2cec14bd7cc29919a03bcd50
andela-rekemezie <rowland.ekemezie@andela.com>
2016-06-14 12:26:03 +0100

    Add sqlalchemy as a dependency in the setup file

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(name='Portfolio',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=[
           'Flask==0.11.1',
+          'MarkupSafe',
+          'SQLAlchemy>=0.8.0',
           'Flask-SQLAlchemy==2.1',
           'Flask-Login==0.3.2',
           'Flask-WTF==0.12'
